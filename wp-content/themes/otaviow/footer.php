d0a744c0871111832d73121333553440dea3f8fa
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2024-03-02 09:40:42 -0300

    add theme support

---
+++ wp-content/themes/otaviow/footer.php
@@ -0,0 +1,14 @@
+    <footer class="site-footer">
+      <div class="container">
+        <div class="copyright">
+          <p>Copyright X - All Rights Reserved</p>
+        </div>
+        <div class="footer-menu">
+          <?php wp_nav_menu( array('theme_location' => 'otavios_footer_menu', 'depth' => 1));?>
+        </div> 
+      </div>
+    </footer>
+  </div>
+  <?php wp_footer();?>
+</body>
+</html>
---
+++ wp-content/themes/otaviow/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes() ?>;">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head()?>
+</head>
+<body <?php body_class()?>>
+
+  <div id="page" class="site">
+    <header>
+      <section class="top-bar">
+        <div class="container">
+          <div class="logo">
+            logo
+          </div>
+          <div class="searchbox">
+            search
+          </div>
+        </div>
+      </section>
+      <section class="menu-area">
+        <div class="container">
+          <nav class="main-menu">
+            <?php wp_nav_menu( array( 'theme_location' => 'otaviow_main_menu', 'depth' => 2));?>
+          </nav>
+        </div>
+      </section>
+    </header>
